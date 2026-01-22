f61edc3bb38af06bf1950a0b6239b7550791d9ba
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2026-01-22 09:38:52 -0700

    week 3 demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,40 @@
 import Image from "next/image";
 import Header from "../components/header";
-import Calculate, { calculateSomething } from "@/components/calculate";
 import { returnUserDetails } from "@/util/simulate_api";
-import NicePictures from "@/components/nice_pictures";
+import DisplayUserDetails from "@/components/displayUserDetails";
+
 import Link from "next/link";
 
 export default function Home() {
-  const results = calculateSomething();
+  const capitalizer = (str: string) => {
+    return str.toUpperCase();
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-red-50 font-sans dark:bg-black w-full">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start px-16 bg-white dark:bg-black sm:items-start">
-        <Header />
-        <div className="py-12">
-          <Image
-            className="dark:invert rounded-2xl shadow-lg border border-red-500"
-            src="/teddy_bear.jpg"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            priority
-          />
-          <Calculate />
-          <div>Welcome {returnUserDetails().name}</div>
-          <NicePictures />
-        </div>
-        <div className="italic text-blue-500 text-3xl">
-          <nav>
-            <Link href="/bowlerhat">Bowlerhat Page</Link>
-          </nav>
-        </div>
-      </main>
+    <div className="flex min-h-screen items-center justify-start flex-col bg-gray-50 font-sans dark:bg-black w-full">
+      <Header />
+      <div className=" w-full p-2 px-4">
+        <DisplayUserDetails
+          name="Felix"
+          appelation="Mr."
+          capitalizer={capitalizer}
+        />
+      </div>
+
+      <div className="py-12 bg-gray-100">
+        <Image
+          className="dark:invert rounded-2xl shadow-lg border border-red-500"
+          src="/teddy_bear.jpg"
+          alt="Next.js logo"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+      <div className="italic text-blue-500 text-3xl">
+        <nav>
+          <Link href="/bowlerhat">Bowlerhat Page</Link>
+        </nav>
+      </div>
     </div>
   );
 }
